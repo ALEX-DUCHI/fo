6548b9f5f7b3a964f557a178eebee21ff82dda0f
ALEX-DUCHI <ae.duchid@uea.edu.ec>
2025-08-28 19:41:27 -0500

    proyecto

---
+++ Documents/NetBeansProjects/fo/src/main/java/com/mycompany/fo/Fo.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.fo;
+
+/**
+ *
+ * @author User
+ */
+public class Fo {
+
+    public static void main(String[] args) {
+        for (int i=10; i>=0;i--){
+        System.out.println(i);
+        }
+        System.out.println("LUZ ROJA LUZ VERDE");
+    }
+}
